0880e8b94057a3d54ca8a4fa6b1f42c057f8d03d
Henrique Silva <henrique.oliveirasilvaismart@gmail.com>
2020-05-23 18:33:23 -0300

    Background estático feito

--- jogo.py
+++ jogo.py
@@ -5,12 +5,16 @@ pygame.init()
 # Variáveis
 largura_janela = 1080
 altura_janela = 540
+largura_background = 1080
+altura_background = 540
 
 window = pygame.display.set_mode((largura_janela,altura_janela))
 pygame.display.set_caption('Mariozinho')
 
+background = pygame.image.load('assets/img/mario_backgroud.png').convert()
+background = pygame.transform.scale(background,(largura_background,altura_background))
 mario_img = pygame.image.load('assets/img/nego_mario.png').convert_alpha()
-mario_img = pygame.transform.scale(mario_img,(128,128))
+mario_img = pygame.transform.scale(mario_img,(64,64))
 
 game = True
 
@@ -20,7 +24,8 @@ while game:
             game = False
 
     window.fill((46, 139, 87))
-    window.blit(mario_img,(32,430))
+    window.blit(background,(0,0))
+    window.blit(mario_img,(32,432))
 
     pygame.display.update()
 
