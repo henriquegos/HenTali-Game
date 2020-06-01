568891279623e9e8c845bb4735947ac70484db10
TalissaGA <56507707+TalissaGA@users.noreply.github.com>
2020-06-01 11:34:45 -0300

    Update heli_game.py

--- heli_game.py
+++ heli_game.py
@@ -1,1 +1,23 @@
+import pygame
+import random 
+from configuracao import largura_janela, altura_janela, GAME, QUIT, INIT, TITULO
+from tela_inicio import init_screen 
+from jogo import screen_game
 
+#pygame.init()inicia framework
+pygame.init()
+
+#Janela
+window = pygame.display.set_mode((largura_janela,altura_janela))
+pygame.display.set_caption(TITULO)
+
+state = INIT 
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = screen_game(window)
+    else:
+        state = QUIT
+
+pygame.quit()
